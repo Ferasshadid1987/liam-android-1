f8c1d9f2c0d3410e71723208b27d03b487935f98
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2020-03-28 09:42:50 +0200

    added repository with mock data

---
+++ app/src/main/java/com/ahmedtikiwa/liam/domain/StoreItem.kt
@@ -0,0 +1,9 @@
+package com.ahmedtikiwa.liam.domain
+
+data class StoreItem(
+    var id : Int?,
+    var name: String?,
+    var price: String?,
+    var type: String?,
+    var imageUrl : String?
+)
---
+++ app/src/main/java/com/ahmedtikiwa/liam/domain/VideoItem.kt
@@ -0,0 +1,9 @@
+package com.ahmedtikiwa.liam.domain
+
+data class VideoItem (
+    var id : Int?,
+    var name: String?,
+    var description : String?,
+    var price : String?,
+    var imageUrl : String?
+)
